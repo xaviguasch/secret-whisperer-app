3fffc5e0b5d6df2123e16f8c978361092df184b9
xaviguasch <xaviguasch@gmail.com>
2019-03-06 20:04:50 +0100

    added password encryption with mongoose-encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
 
 const app = express()
 
@@ -16,10 +17,17 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
     useNewUrlParser: true
 })
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+})
+
+const secret = 'Thisisourlittlesecret.'
+
+userSchema.plugin(encrypt, {
+    secret: secret,
+    encryptedFields: ['password']
+})
 
 const User = new mongoose.model('User', userSchema)
 
@@ -70,7 +78,6 @@ app.post('/login', function (req, res) {
             }
         }
     })
-
 })
 
 
